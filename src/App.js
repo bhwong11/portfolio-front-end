d7143d7e59b20ed5acb306056122e79a1c93633b
bhwong11 <user@email.com>
2021-09-19 21:30:59 -0400

    added portfolio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from "react";
+
 
 function App() {
+  const [test,setData] = useState(null)
+  useEffect(
+    function(){
+      fetch('http://localhost:4000/jsontest')
+      .then(response=>{
+        console.log(response)
+        return response.json()
+      }
+        )
+      .then(data=>
+        {
+          setData(data)
+        })
+    },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+         test?(
+          <h1>{test.test}</h1>
+        ):(
+          <h3>loading...</h3>
+        )
+      }
     </div>
   );
 }

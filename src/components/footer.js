452bf53c9e19eea3dad0401f065e11fb4d3ba502
bhwong11 <user@email.com>
2021-09-28 23:01:35 -0400

    added github icon and linkedin icon

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,14 @@
 import React,{useState,useEffect} from 'react';
+import { FaGithubSquare,FaLinkedinIn } from "react-icons/fa";
 import "../css/footer.css"
 
 function Footer(){
 
     return(
         <footer id = "footer">
-            <div>linkedin</div>
-            <div>Github</div>
-            <div>Email</div>
+            <a href="https://github.com/bhwong11" target="_blank"><FaGithubSquare/></a>
+            <a href="https://www.linkedin.com/in/bryanthwong" target="_blank"><FaLinkedinIn/></a>
+            <div className="footer__email">Bryanthwong@gmail.com</div>
         </footer>
     )
 }

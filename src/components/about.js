e1903b97fdae620659bef408c7b1eff184682d99
bhwong11 <user@email.com>
2021-09-30 11:53:59 -0400

    added animation for about section

--- src/components/about.js
+++ src/components/about.js
@@ -5,7 +5,7 @@ function About(){
 
     return(
         <div id = "about" className="hero-body">
-            <div class="about__description">
+            <div class="about__description" data-aos="fade-up">
                 <div class="about__section_title">Nice To Meet You! I'm Bryant</div>
                 I am a software engineer with experience in JavaScript, CSS, REACTjs, the Django Stack, and the MERN Stack. My background in digital marketing has given me an eye for detail and experience with reading documentation to learn new technologies. I am passionate about problem solving, love testing and learning and am looking to translate this passion into a new position in the tech sector.
             </div>
--- src/components/resume.js
+++ src/components/resume.js
@@ -5,7 +5,7 @@ function Resume(){
 
     return(
         <div id = "resume" className="hero-body">
-            <h3 className="resume__title">Want To Work Togther?</h3>
+            <h3 className="resume__title" >Want To Work Togther?</h3>
             <a className="resume_link button is-outlined" href="/bryant_wong_resume.pdf" target="_blank" rel="noreferrer">View Resume</a>
         </div>
     )
